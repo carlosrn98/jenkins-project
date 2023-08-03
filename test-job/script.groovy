724fa66155fdb393950a6fb0f87456b4203f68e4
carlosrn98 <carlosrn98@gmail.com>
2023-08-03 17:01:32 -0600

    Add new shared library and Jenkins test job script
        This change adds the shared library and updates the test job to be
        compliant with the job defined in the shared library.

--- test-job/script.groovy
+++ test-job/script.groovy
@@ -1,12 +1,17 @@
-pipeline {
-    agent {
-        label "slave1"
-    }
-    stages {
-        stage("Test that it is working") {
-            steps {
-                sh "echo 'The pipeline works!'"
-            }
-        }
-    }
+// pipeline {
+//     agent {
+//         label "slave1"
+//     }
+//     stages {
+//         stage("Test that it is working") {
+//             steps {
+//                 sh "echo 'The pipeline works!'"
+//             }
+//         }
+//     }
+// }
+library 'jenkins-shared-library@pipelines'
+
+testPipeline {
+    agent = 'slave1'
 }
